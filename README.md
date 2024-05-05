2e3b06e6f9ff4a24eb68cff9c9c84cf4c16d41a7
Marcus Kraßmann <marcus.krassmann@gmail.com>
2024-05-05 16:15:00 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,2 @@
+# Robotmon monitor
+This acts as a script callback endpoint to ensure running Robotmon scripts and restart their VMs in case a script does not call the endpoint within a given time.
---
+++ main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const restartThreshold = 10 * time.Minute // Time threshold for restarting the process
+
+var (
+	port            int
+	processCommand  string
+	processTitle    string
+	lastRequestTime time.Time
+	processID       int
+	debugLogs       bool
+)
+
+func init() {
+	flag.StringVar(&processCommand, "process", "", "Name of the process to manage")
+	flag.IntVar(&port, "port", 0, "Port number to listen on")
+	flag.StringVar(&processTitle, "title", "", "Title of the target window")
+	flag.BoolVar(&debugLogs, "debug", false, "Log all HTTP requests on console?")
+	flag.Parse()
+
+	if processCommand == "" || port == 0 {
+		fmt.Println("Both -process and -port parameters are required")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	logDebug("Startup successful")
+}
+
+/*
+Example call:
+main.exe -debug=true -port=8888 -process="C:\LDPlayer\LDPlayer4.0\dnplayer.exe index=1" -title "TsumTsum Alice"
+*/
+func main() {
+	startProcessIfNotRunning()
+
+	http.HandleFunc("/", handler)
+	go monitorAndRestartProcess()
+
+	logInfo("Server started on port %s", strconv.Itoa(port))
+	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	if err != nil {
+		fmt.Println("HTTP serve error:", err)
+		os.Exit(1)
+	}
+}
+
+func handler(w http.ResponseWriter, _ *http.Request) {
+	_, err := fmt.Fprintf(w, "OK")
+	if err != nil {
+		fmt.Println("HTTP handler error:", err)
+		os.Exit(1)
+	}
+	logDebug("received GET")
+	lastRequestTime = time.Now()
+}
+
+func logInfo(msg string, arg ...any) {
+	fmt.Printf("%s - "+msg+"\n", time.Now().Format(time.DateTime), arg)
+}
+
+func logDebug(msg string) {
+	if debugLogs {
+		logInfo(msg)
+	}
+}
+
+func startProcessIfNotRunning() {
+	logInfo("Title = %s", processTitle)
+	if processTitle != "" {
+		filterQuery := fmt.Sprintf("WINDOWTITLE eq %s", processTitle)
+		stdout, err := exec.Command("cmd", "/C", "tasklist", "/FI", filterQuery, "/FO", "CSV", "/NH").CombinedOutput()
+		if err != nil {
+			log.Fatal("Unable to start tasklist command", err)
+		} else {
+			csvReader := csv.NewReader(strings.NewReader(string(stdout[:])))
+			records, err := csvReader.ReadAll()
+			if err != nil {
+				log.Fatal("Unable to parse file as CSV", err)
+			}
+			if len(records) > 1 {
+				log.Fatal("Found more than one process with window title", processTitle)
+			}
+			if len(records) == 1 {
+				line := records[0]
+				if len(line) >= 2 {
+					processIdStr := line[1]
+					processIdInt, err := strconv.Atoi(processIdStr)
+					if err != nil {
+						log.Fatal("Unable to fetch processId from tasklist", err)
+					}
+					if processIdInt > 0 && processIdInt < 65536 {
+						logInfo("Found already running process with id %d. Not starting a new process", processIdInt)
+						processID = processIdInt
+					}
+				}
+			}
+		}
+	}
+	if processID == 0 {
+		command := strings.Fields(processCommand)
+		logInfo("Start command: %s", processCommand)
+		cmd := exec.Command(command[0], command[1:]...)
+		err := cmd.Start()
+		if err != nil {
+			log.Fatal("Error starting process:", err)
+		}
+		processID = cmd.Process.Pid
+		logInfo("Process started with PID %s", strconv.Itoa(processID))
+	}
+	lastRequestTime = time.Now().Add(-restartThreshold + 5*time.Minute) // first message needs to be sent within 5 minutes
+}
+
+func monitorAndRestartProcess() {
+	for {
+		time.Sleep(1 * time.Minute)
+
+		if time.Since(lastRequestTime) > restartThreshold {
+			logInfo("No requests were received for more than %s. Restarting process...", restartThreshold.String())
+
+			// Kill process
+			killCmd := exec.Command("taskkill", "/F", "/PID", strconv.Itoa(processID))
+			err := killCmd.Run()
+			processID = 0
+			if err != nil {
+				fmt.Println("Error killing process:", err)
+			}
+
+			// Wait 10 seconds
+			time.Sleep(10 * time.Second)
+
+			// Start process
+			startProcessIfNotRunning()
+
+			lastRequestTime = time.Now()
+		} else if time.Since(lastRequestTime) > restartThreshold-1*time.Minute {
+			logInfo("WARNING: No requests were received for more than %s. Restarting process in one minute if still no request happened...", (restartThreshold - 1*time.Minute).String())
+		}
+	}
+}
