433233ff61fb4f2baf851b5e4cade3f2762384d4
Marcus Kraßmann <marcus.krassmann@gmail.com>
2024-07-19 16:42:41 +0200

    Fix logging for logs without formatting string

--- main.go
+++ main.go
@@ -69,7 +69,11 @@ func handler(w http.ResponseWriter, _ *http.Request) {
 }
 
 func logInfo(msg string, arg ...any) {
-	fmt.Printf("%s - "+msg+"\n", time.Now().Format(time.DateTime), arg)
+	if len(arg) > 0 {
+		fmt.Printf("%s - "+msg+"\n", time.Now().Format(time.DateTime), arg)
+	} else {
+		fmt.Printf("%s - "+msg+"\n", time.Now().Format(time.DateTime))
+	}
 }
 
 func logDebug(msg string) {
